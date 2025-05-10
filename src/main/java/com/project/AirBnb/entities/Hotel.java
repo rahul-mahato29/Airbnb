ee7a42c7280cd952ed4cf09920c9eaa293d6f537
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-10 20:33:02 +0530

    added Hotel and HotelContactInfo entities

---
+++ src/main/java/com/project/AirBnb/entities/Hotel.java
@@ -0,0 +1,43 @@
+package com.project.AirBnb.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @Embedded
+    private HotelContactInfo contactInfo;
+
+    private boolean isActive;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
