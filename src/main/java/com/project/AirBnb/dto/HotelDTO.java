3780de3e663df4f1968678816a11f276479a7f1a
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-16 00:49:32 +0530

    added Hotel and Room DTO

--- src/main/java/com/project/AirBnb/dto/HotelDTO.java
+++ src/main/java/com/project/AirBnb/dto/HotelDTO.java
@@ -1,4 +1,13 @@
 package com.project.AirBnb.dto;
 
+import com.project.AirBnb.entities.HotelContactInfo;
+
 public class HotelDTO {
+    private Long id;
+    private String name;
+    private String city;
+    private String[] photos;
+    private String[] amenities;
+    private HotelContactInfo contactInfo;
+    private Boolean isActive;
 }
--- src/main/java/com/project/AirBnb/dto/RoomDTO.java
+++ src/main/java/com/project/AirBnb/dto/RoomDTO.java
@@ -1,4 +1,16 @@
 package com.project.AirBnb.dto;
 
+import com.project.AirBnb.entities.Hotel;
+
+import java.math.BigDecimal;
+
 public class RoomDTO {
+    private Long id;
+    private Hotel hotel;
+    private String type;
+    private BigDecimal basePrice;
+    private String[] photos;
+    private String[] amenities;
+    private Integer totalCount;
+    private Integer capacity;
 }
