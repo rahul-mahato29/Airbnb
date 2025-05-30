9145745a42148a6935874b59c4a5aecba279c6ed
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-30 23:12:32 +0530

    booking

---
+++ src/main/java/com/project/AirBnb/controllers/HotelBookingController.java
@@ -0,0 +1,12 @@
+package com.project.AirBnb.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/Booking")
+@RequiredArgsConstructor
+public class HotelBookingController {
+
+}
---
+++ src/main/java/com/project/AirBnb/services/Impl/BookingServiceImpl.java
@@ -0,0 +1,6 @@
+package com.project.AirBnb.services.Impl;
+
+import com.project.AirBnb.services.BookingService;
+
+public class BookingServiceImpl implements BookingService {
+}
