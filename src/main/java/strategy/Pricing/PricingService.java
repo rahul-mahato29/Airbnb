5ce4d1ee9770f48e30399e27475546f41d085826
rahul-mahato29 <mahatorahul2901@gmail.com>
2025-06-09 22:52:42 +0530

    added all Pricing Strategies

---
+++ src/main/java/strategy/Pricing/PricingService.java
@@ -0,0 +1,24 @@
+package strategy.Pricing;
+
+import com.project.AirBnb.entities.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class PricingService {
+    public BigDecimal calculateDynamicPricing(Inventory inventory) {
+        PricingStrategy pricingStrategy = new BasePricingStrategy();
+
+        //apply the additional strategies
+        pricingStrategy = new SurgePricingStrategy(pricingStrategy);
+        pricingStrategy = new OccupancyPricingStrategy(pricingStrategy);
+        pricingStrategy = new UrgencyPricingStrategy(pricingStrategy);
+        pricingStrategy = new HolidayPricingStrategy(pricingStrategy);
+
+        return pricingStrategy.calculatePrice(inventory);
+    }
+
+}
